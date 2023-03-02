f82e64501897e7da93d29bd38a4c1a0ec949f10b
Bruno Vergasta Paiva <ppaiva11144@gmail.com>
2023-03-02 23:38:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>HiberWhisper</title>
+  <!-- Chargement de Tailwind CSS -->
+  <script src="https://cdn.tailwindcss.com"></script>
+  <!-- Chargement de SweetAlert2 -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+  <!-- Chargement de la police Ohno Blazeface -->
+  <link rel="stylesheet" href="https://use.typekit.net/ieo2idj.css" />
+</head>
+
+<body>
+  <!-- Header (only logo on the top left) (anchered) -->
+  <header class="fixed">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <img src="logo.png" alt="HiberWhisper" class="h-12 w-auto" />
+    </div>
+  </header>
+  <div class="min-h-screen bg-blue-200 flex items-center justify-center">
+    <div class="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md">
+      <h1 class="text-2xl mb-4" style="font-family: ohno-blazeface">
+        HiberWhisper
+      </h1>
+      <hr />
+      <p class="mb-4 mt-4">
+        Service de transcription <u>gratuit</u> d'audio qui convertit
+        n'importe quel fichier audio MP3 en texte grâce à l'API d'OpenAI
+        (Whisper).
+      </p>
+      <form id="upload-form" class="mb-4" action="traitement.php" method="post" enctype="multipart/form-data">
+        <div class="mb-4">
+          <label for="audio-file" class="block text-gray-700 font-bold mb-2">Fichier audio (mp3) :</label>
+          <div class="relative rounded-md shadow-sm">
+            <input type="file" name="audio-file" id="audio-file" class="opacity-0 absolute inset-0 z-50" required />
+            <div class="flex items-center justify-center px-4 py-2 bg-white border border-gray-300 rounded-md font-medium text-gray-700 hover:bg-gray-50 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500 cursor-pointer">
+              <span class="custom-file-label">Choisir un fichier</span>
+            </div>
+          </div>
+        </div>
+        <button id="submit-button" type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          HiberWhisperiser !
+        </button>
+      </form>
+      <div id="loading-container" class="hidden flex items-center justify-center">
+        <div class="rounded-full p-2 border-blue-600 border-4">
+          <div class="w-4 h-4 bg-blue-600 rounded-full animate-ping"></div>
+        </div>
+        <div class="ml-4 text-gray-700 font-medium">HiberWhisperisation en cours...</div>
+      </div>
+      <div id="result-container" class="hidden">
+        <h2 class="text-lg font-bold mb-2">
+          Résultat de la HiberWhisperisation :
+        </h2>
+        <div id="result-text" class="bg-blue-100 p-4 rounded max-h-96 overflow-y-auto"></div>
+        <button id="copy-button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+          Copier
+        </button>
+        <button id="return-button" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-4">
+          Retour
+        </button>
+      </div>
+    </div>
+  </div>
+  <footer class="fixed bottom-8 left-8 h-8 w-full text-white text-sm flex items-center justify-start">
+    <div class="px-4">© 2023 HiberFile Team</div>
+  </footer>
+  <script>
+    const input = document.getElementById('audio-file');
+    const label = document.querySelector('.custom-file-label');
+
+    input.addEventListener('change', () => {
+      const fileName = input.files[0].name;
+      label.textContent = fileName;
+    });
+
+    // Show loading screen when form is submitted
+    document.getElementById("upload-form").addEventListener("submit", function(e) {
+      e.preventDefault();
+      document.getElementById("upload-form").classList.add("hidden");
+      document.getElementById("loading-container").classList.remove("hidden");
+      this.submit();
+    });
+  </script>
+</body>
+
+</html>
---
+++ traitement.php
@@ -0,0 +1,145 @@
+<?php
+
+// Do not show any errors
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
+
+$result = "";
+
+// Check if POST content length is too large
+if ($_SERVER['CONTENT_LENGTH'] > 10000000) {
+    http_response_code(400);
+    // Show error message in $result and exit
+    $result = "Le fichier audio est trop volumineux. La taille maximale est de 10 Mo.";
+}
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Récupération de la clé d'API depuis une variable d'environnement
+$api_key = $_ENV['API_KEY'];
+
+// Vérification de la présence d'un fichier audio
+if (!isset($_FILES['audio-file'])) {
+    http_response_code(400);
+    $result = "Aucun fichier audio n'a été envoyé.";
+}
+
+// Récupération du fichier audio et vérification de son type
+$file = $_FILES['audio-file'];
+if ($file['error'] !== UPLOAD_ERR_OK) {
+    http_response_code(400);
+    $result = "Erreur lors de l'envoi du fichier : " . $file['error'];
+}
+if ($file['type'] !== 'audio/mpeg') {
+    http_response_code(400);
+    $result = "Le type de fichier " . $file['type'] . " n'est pas supporté. Le fichier doit être de type audio/mpeg.";
+}
+
+// Préparation de la requête API
+$client = new Client(['headers' => ['Authorization' => 'Bearer ' . $api_key]]);
+$url = 'https://api.openai.com/v1/audio/transcriptions';
+$options = [
+    'multipart' => [
+        [
+            'name' => 'file',
+            'contents' => fopen($file['tmp_name'], 'r'),
+            'filename' => $file['name'],
+        ],
+        [
+            'name' => 'model',
+            'contents' => 'whisper-1',
+        ],
+    ],
+];
+
+// Envoi de la requête API et récupération du résultat (mais seulement si le fichier audio est valide)
+if ($result === "") {
+    try {
+        $result = json_decode($client->post($url, $options)->getBody(), true)['text'];
+    } catch (RequestException $e) {
+        http_response_code(400);
+        $result = "Erreur lors de la transcription de l'audio : " . $e->getMessage();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>HiberWhisper</title>
+    <!-- Chargement de Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Chargement de SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <!-- Chargement de la police Ohno Blazeface -->
+    <link rel="stylesheet" href="https://use.typekit.net/ieo2idj.css" />
+</head>
+
+<body>
+    <!-- Header (only logo on the top left) (anchered) -->
+    <header class="fixed">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <img src="logo.png" alt="HiberWhisper" class="h-12 w-auto" />
+        </div>
+    </header>
+    <div class="min-h-screen bg-blue-200 flex items-center justify-center">
+        <div class="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md">
+            <h1 class="text-2xl mb-4" style="font-family: ohno-blazeface">
+                HiberWhisper
+            </h1>
+            <hr />
+            <p class="mb-4 mt-4">
+                Service de transcription <u>gratuit</u> d'audio qui convertit
+                n'importe quel fichier audio MP3 en texte grâce à l'API d'OpenAI
+                (Whisper).
+            </p>
+            <div id="result-container">
+                <h2 class="text-lg font-bold mb-2">
+                    Résultat de la HiberWhisperisation :
+                </h2>
+                <div id="result-text" class="bg-blue-100 p-4 rounded max-h-96 overflow-y-auto">
+                    <?php echo $result; ?>
+                </div>
+                <button id="copy-button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+                    Copier
+                </button>
+                <button id="return-button" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded mt-4">
+                    Retour
+                </button>
+            </div>
+        </div>
+    </div>
+    <script>
+        // Copie le texte transcrit dans le presse-papier
+        const copyButton = document.getElementById("copy-button");
+        copyButton.addEventListener("click", () => {
+            const resultText = document.getElementById("result-text");
+            const text = resultText.innerText;
+            navigator.clipboard.writeText(text);
+            Swal.fire({
+                title: "Copié !",
+                text: "Le texte a été copié dans le presse-papiers",
+                icon: "success",
+                confirmButtonText: "OK",
+            });
+        });
+        // Retour à la page d'accueil
+        const returnButton = document.getElementById("return-button");
+        returnButton.addEventListener("click", () => {
+            window.location.href = "index.php";
+        });
+    </script>
+</body>
+
+</html>
