8253b183e3731b565bc8b959f16feb6a7809b1cb
Bruno Vergasta Paiva <ppaiva11144@gmail.com>
2023-03-03 01:08:25 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -7,59 +7,67 @@ error_reporting(0);
 
 $result = "";
 
+// Check if POST content length is too large
+if ($_SERVER['CONTENT_LENGTH'] > 10000000) {
+    http_response_code(400);
+    // Show error message in $result and exit
+    $result = "Le fichier audio est trop volumineux. La taille maximale est de 10 Mo.";
+}
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Récupération de la clé d'API depuis une variable d'environnement
+$api_key = $_ENV['API_KEY'];
+
 // Vérification de la présence d'un fichier audio
 if (!isset($_FILES['audio-file'])) {
     http_response_code(400);
     $result = "Aucun fichier audio n'a été envoyé.";
-} else {
-    // Récupération du fichier audio et vérification de son type
-    $file = $_FILES['audio-file'];
-    if ($file['error'] !== UPLOAD_ERR_OK) {
+}
+
+// Récupération du fichier audio et vérification de son type
+$file = $_FILES['audio-file'];
+if ($file['error'] !== UPLOAD_ERR_OK) {
+    http_response_code(400);
+    $result = "Erreur lors de l'envoi du fichier : " . $file['error'];
+}
+if ($file['type'] !== 'audio/mpeg') {
+    http_response_code(400);
+    $result = "Le type de fichier " . $file['type'] . " n'est pas supporté. Le fichier doit être de type audio/mpeg.";
+}
+
+// Préparation de la requête API
+$client = new Client(['headers' => ['Authorization' => 'Bearer ' . $api_key]]);
+$url = 'https://api.openai.com/v1/audio/transcriptions';
+$options = [
+    'multipart' => [
+        [
+            'name' => 'file',
+            'contents' => fopen($file['tmp_name'], 'r'),
+            'filename' => $file['name'],
+        ],
+        [
+            'name' => 'model',
+            'contents' => 'whisper-1',
+        ],
+    ],
+];
+
+// Envoi de la requête API et récupération du résultat (mais seulement si le fichier audio est valide)
+if ($result === "") {
+    try {
+        $result = json_decode($client->post($url, $options)->getBody(), true)['text'];
+    } catch (RequestException $e) {
         http_response_code(400);
-        $result = "Erreur lors de l'envoi du fichier : " . $file['error'];
-    } elseif ($file['type'] !== 'audio/mpeg') {
-        http_response_code(400);
-        $result = "Le type de fichier " . $file['type'] . " n'est pas supporté. Le fichier doit être de type audio/mpeg.";
-    } else {
-        // Préparation de la requête API
-        $file_path = $file['tmp_name'];
-        if (!empty($file_path)) {
-            require 'vendor/autoload.php';
-
-            use GuzzleHttp\Client;
-            use GuzzleHttp\Exception\RequestException;
-
-            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-            $dotenv->load();
-
-            $api_key = $_ENV['API_KEY'];
-            $client = new Client(['headers' => ['Authorization' => 'Bearer ' . $api_key]]);
-            $url = 'https://api.openai.com/v1/audio/transcriptions';
-            $options = [
-                'multipart' => [
-                    [
-                        'name' => 'file',
-                        'contents' => fopen($file_path, 'r'),
-                        'filename' => $file['name'],
-                    ],
-                    [
-                        'name' => 'model',
-                        'contents' => 'whisper-1',
-                    ],
-                ],
-            ];
-
-            // Envoi de la requête API et récupération du résultat
-            try {
-                $result = json_decode($client->post($url, $options)->getBody(), true)['text'];
-            } catch (RequestException $e) {
-                http_response_code(400);
-                $result = "Erreur lors de la transcription de l'audio : " . $e->getMessage();
-            }
-        } else {
-            http_response_code(400);
-            $result = "Le chemin de fichier est vide.";
-        }
+        $result = "Erreur lors de la transcription de l'audio : " . $e->getMessage();
     }
 }
 ?>
